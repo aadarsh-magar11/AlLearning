af601994dc4a9026c4c6ebd83a1b9791464dd690
unknown <aadarsha1234thapa@gmail.com>
2025-12-03 14:57:13 +0545

    updated

--- EXTRAPROJECTS/BEGINNER/ExpenseCal.py
+++ EXTRAPROJECTS/BEGINNER/ExpenseCal.py
@@ -1,34 +1,56 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import csv
-read=pd.read_csv("expense.csv")
-day=np.array(["sunday","monday","tuesday","wednesday","thursday","firday","saturday"])
+day=np.array(["sunday","monday","tuesday","wednesday","thursday","friday","saturday"])
+ 
+ #for piechart
+cause=np.array([])
+kharcha=np.array([])
 
-def add(x, y, z):
+df_exists=pd.read_csv("expense.csv")#reading csv for the last week's number
+if len(df_exists) == 0:
+    week=1;
+else:
+    week= df_exists["week"].max() + 1;
+
+
+def add(m, x, y, z):#function to add new data to csv file
     with open("expense.csv", "a", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow([x, y, z])
+        writer.writerow([m, x, y, z])
 
-n=0
+
+n=0#initializing for array of week
 for x in range(0,7):#for looping between days
     din=day[n]
+    print(f"for week {week}")
     print(f"for {day[n]}")
     need=input("enter the cause:")
     expense=int(input("enter the cost:"))
-    add(din,need,expense)
+
+    cause=np.append(cause,need)#appending in array
+    kharcha=np.append(kharcha,expense)
+
+    add(week,din,need,expense)
     more=(input(f"add more for {day[n]}?:")).lower()
     while more=="yes":
         need=input("enter the cause:")
         expense=int(input("enter the cost"))
-        add(din,need,expense)
+
+        cause=np.append(cause,need)#appending in array
+        kharcha=np.append(kharcha,expense)
+
+        add(week,din,need,expense)
         more=(input(f"add more for {day[n]}?:")).lower()
         break
     n=n+1
     
-#calculating total expense
-def expense(rd):
-    df=pd.DataFrame(rd)
-    total_cost=df["cost"].sum()
-    print(f"the total expense of the week is {total_cost}")
+#to print the sum of the week expense
+df = pd.read_csv("expense.csv")
+week_total = df[df["week"] == week]["cost"].sum()
+print(f"\nTotal expense for Week {week}: {week_total}")
 
-expense(read)
+#plotting piechart
+plt.pie(kharcha, labels=cause, autopct="%1.1f%%")
+plt.show()
